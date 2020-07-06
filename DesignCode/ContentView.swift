b7ffdff3b8e1e6793bc7f3089193df9cd68b212d
Harry Twan <gua@desgard.com>
2020-07-06 23:27:32 +0800

    finish the P4 Animations and States
    Signed-off-by: Harry Twan <gua@desgard.com>

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -8,46 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var show = false
+    
+    
     var body: some View {
         ZStack {
             
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
 
             BackCardView()
-                .frame(width: 340, height: 220)
-                .background(Color("background3"))
+                .background(show ? Color("background6") : Color("background3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -40.0)
+                .offset(x: 0, y: show ? -400 : -40.0)
                 .scaleEffect(0.9)
-                .rotationEffect(.degrees(10))
+                .rotationEffect(.degrees((show ? 0 : 10)))
                 .rotation3DEffect(
                     Angle(degrees: 10),
                     axis: (x: 10.0, y: 0.0, z: 0.0)
                 )
                 .blendMode(.hardLight)
+                .animation(.easeIn(duration: 0.5))
             
             BackCardView()
-                .frame(width: 340, height: 220)
-                .background(Color("background10"))
+                .background(show ? Color("background6") : Color("background10"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20.0)
+                .offset(x: 0, y: show ? -200 : -20.0)
                 .scaleEffect(0.95)
-                .rotationEffect(.degrees(5))
+                .rotationEffect(.degrees(show ? 0 : 5))
                 .rotation3DEffect(
                     Angle(degrees: 5),
                     axis: (x: 10.0, y: 0.0, z: 0.0)
                 )
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
             
             CardView()
                 .blendMode(.hardLight)
-            
+                .onTapGesture {
+                    show.toggle()
+                }
             
             BottomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
         }
     }
 }
@@ -96,7 +104,7 @@ struct BackCardView: View {
         VStack {
             Spacer()
         }
-        
+        .frame(width: 340, height: 220)
     }
 }
 
