0d6344102e70b0a3746ece2eeccff78e1093ddb1
Harry Twan <gua@desgard.com>
2020-07-09 19:23:57 +0800

    finish the Animation Between Screens
    Signed-off-by: Harry Twan <gua@desgard.com>

---
+++ DesignCode/Home.swift
@@ -0,0 +1,66 @@
+//
+//  Home.swift
+//  DesignCode
+//
+//  Created by Harry Twan on 2020/7/9.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    @State var showProfile = false
+    
+    var body: some View {
+        ZStack {
+            
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("Watching")
+                        .font(.system(size: 20, weight: .bold))
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        showProfile.toggle()
+                    }) {
+                        Image("avatar")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 36, height: 36)
+                            .clipShape(Circle())
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+                
+                Spacer()
+            }
+            .padding(.top, 44)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset(y: showProfile ? -450 : 0)
+            .rotation3DEffect(
+                Angle(degrees: showProfile ? -10 : 0),
+                axis: (x: 10.0, y: 0, z: 0)
+            )
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .edgesIgnoringSafeArea(.all)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            
+            MenuView()
+                .offset(y: showProfile ? 0 : 600)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
