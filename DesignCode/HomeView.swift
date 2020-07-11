4850eb6e53909e66b74141cd52fadd6baa748763
Harry Twan <gua@desgard.com>
2020-07-11 19:02:29 +0800

    finish the Modal Presentation
    Signed-off-by: Harry Twan <gua@desgard.com>

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -11,6 +11,8 @@ struct HomeView: View {
     
     @Binding var showProfile: Bool
     
+    @State var showUpdate = false
+    
     var body: some View {
         VStack {
             HStack {
@@ -20,6 +22,23 @@ struct HomeView: View {
                 Spacer()
                 
                 AvatarView(showProfile: $showProfile)
+                
+                Button(action: {
+                    showUpdate.toggle()
+                }) {
+                    Image(systemName: "bell")
+                        .renderingMode(.original)
+                        .font(.system(size: 16, weight: .medium))
+                        .frame(width: 36, height: 36)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                        
+                }
+                .sheet(isPresented: $showUpdate) {
+                    ContentView()
+                }
             }
             .padding(.horizontal)
             .padding(.leading, 14)
