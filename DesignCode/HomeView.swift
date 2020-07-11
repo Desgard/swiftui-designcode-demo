5d1030711e5d7318b72775a9254cf0a8ad710db7
Harry Twan <gua@desgard.com>
2020-07-11 18:09:47 +0800

    finish the Data and Image Literal
    Signed-off-by: Harry Twan <gua@desgard.com>

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -22,16 +22,17 @@ struct HomeView: View {
                 AvatarView(showProfile: $showProfile)
             }
             .padding(.horizontal)
+            .padding(.leading, 14)
             .padding(.top, 30)
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
-                    ForEach(0 ..< 5) { _ in
-                        SectionView()
+                    ForEach(sectionData) { item in
+                        SectionView(section: item)
+
                     }
                 }
                 .padding(30)
-                .padding(.leading, 14)
                 .padding(.bottom, 30)
             }
 
@@ -47,20 +48,22 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct SectionView: View {
+    var section: Section
+    
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("Prototype designs in SwiftUI")
+                Text(section.title)
                     .font(.system(size: 24, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(.white)
                 Spacer()
-                Image("Logo")
+                Image(section.logo)
             }
             
-            Text("18 Sections".uppercased())
+            Text(section.text.uppercased())
                 .frame(maxWidth: .infinity, alignment: .leading)
-            Image("Certificate1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -68,13 +71,28 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("background3"))
+        .background(section.color)
         .cornerRadius(30)
         .shadow(
-            color: Color("background3").opacity(0.3),
+            color: section.color.opacity(0.3),
             radius: 20,
             x: 0,
             y: 20
         )
     }
 }
+
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo", image: Image("Certificate1"), color: Color("background3")),
+    Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo", image: Image("Certificate1"), color: Color("background4")),
+]
